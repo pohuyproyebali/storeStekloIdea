96f7bc9c6694eae39938023e63c5d94b95b06630
Nikolay Lubochnikov <mrkololoshka413@gmail.com>
2023-12-11 17:14:58 +0300

    Настроил Docker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.11-slim
+
+RUN mkdir code
+WORKDIR code
+
+ADD . /code/
+ADD .env.docker /code/.env
+
+ENV APP_NAME=STEKLO_IDEA
+
+RUN pip install -r requirements.txt
+RUN python manage.py loaddata products.json
+
+CMD gunicorn storeStekloIdea.wsgi:application -b 0.0.0.0:8000
--- storeStekloIdea/settings.py
+++ storeStekloIdea/settings.py
@@ -10,20 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mkdam)5=jz57xrsz6t7w=^35fks-_jjca-hxdjxgc2fl7f1vgf'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
