80010788b69733cc186498b0d173f2093b8d1ef3
Nikolay Lubochnikov <mrkololoshka413@gmail.com>
2023-12-18 13:10:44 +0300

    Модели для главной страницы, исправление header для главной страницы и страницы каталога. И другие незначительные изменения.

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from pages.models import *
 
 
 # Create your views here.
+
 def main_page(request):
     context = {
+        'image_next_to_the_slogan': ImageForPage.objects.get(
+            image_type=ImageType.objects.get(name="Image next to the slogan on main page").id
+        ).image.url,
         'for_header': True
     }
     return render(request, 'index.html', {'context': context})
